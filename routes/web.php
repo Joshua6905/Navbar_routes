50806408bf4f01149677c1e06ecc4b72bed143e6
Joshua6905 <estradajf.1105@gmail.com>
2023-11-18 14:31:36 +0800

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,13 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+// Importing the controllers
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SettingsController;
+use App\Http\Controllers\ContactUsController;
+use App\Http\Controllers\AboutUsController;
+use App\Http\Controllers\NewsController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,18 +19,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
+// Routes with controller
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+Route::get('/settings', [SettingsController::class, 'index'])->name('settings');
+Route::get('/contactus', [ContactUsController::class, 'index'])->name('contactus');
+Route::get('/aboutus', [AboutUsController::class, 'index'])->name('aboutus');
+Route::get('/news', [NewsController::class, 'index'])->name('news');
